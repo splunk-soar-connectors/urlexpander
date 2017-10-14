4cf71d26bd41a2f9de2096f55416abc976cfc966
phantomqa <david@phantom.us>
2017-10-14 09:08:13 -0700

    initial commit

---
+++ urlexpander_consts.py
@@ -0,0 +1,15 @@
+# --
+# File: urlexpander_consts.py
+# --
+URLEXPANDER_JSON_BITLY_SERVER = "bitly_server"
+URLEXPANDER_JSON_BITLY_APIKEY = "bitly_apikey"
+URLEXPANDER_JSON_GOOGL_SERVER = "googl_server"
+URLEXPANDER_JSON_GOOGL_APIKEY = "googl_apikey"
+
+URLEXPANDER_ERR_QUERY = "Whois query failed"
+URLEXPANDER_SUCC_QUERY = "Whois query successful"
+URLEXPANDER_ERR_QUERY_RETURNED_NO_DATA = "Whois query did not return any information"
+URLEXPANDER_ERR_SERVER_CONNECTION = "Connection to server failed"
+URLEXPANDER_ERR_CONNECTIVITY_TEST = "Connectivity test failed"
+URLEXPANDER_SUCC_CONNECTIVITY_TEST = "Connectivity test passed"
+URLEXPANDER_URL = "short URL"
